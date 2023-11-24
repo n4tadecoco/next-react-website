b28d72cef22ae1a10d32ef2aafebc7d2974f0743
n4tadecoco <t.yuya0812@icloud.com>
2023-11-24 16:31:10 +0900

    Create sample.js

---
+++ pages/sample.js
@@ -0,0 +1,14 @@
+import styles from 'styles/sample.module.css'
+
+export default function Sample() {
+    return (
+        <h3 
+            style={{
+                color: 'green',
+                fontSize: '2em',
+            }}
+            >
+                記事のタイトル
+        </h3>
+    )
+}
