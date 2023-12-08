592b8436fde2febf2ea34c79ca02f11716436233
n4tadecoco <t.yuya0812@icloud.com>
2023-12-08 16:31:31 +0900

    Update logo.js

--- components/logo.js
+++ components/logo.js
@@ -1,9 +1,10 @@
 import Link from 'next/Link'
+import styles from 'styles/logo.module.css'
 
-export default function Logo() {
+export default function Logo({ box0n = false }) {
     return (
         <Link href="/">
-            CUBE
+            <a className={box0n ? styles.box : styles.basic}>CUBE</a>
         </Link>
     )
 }
