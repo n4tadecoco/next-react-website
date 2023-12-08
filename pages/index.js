f1d7ec18c57cb2d287d8920bafb4fdb4186562e1
n4tadecoco <t.yuya0812@icloud.com>
2023-12-08 16:31:42 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home() {
   return (
-    <Hero 
-      title="CUBE"
-    subtitle="アウトプットしていくサイト"
-    image0n
-    />
+    <Container>
+    <Hero title="CUBE" subtitle="アウトプットしていくサイト" image0n/>
+    </Container>
   )
 }
