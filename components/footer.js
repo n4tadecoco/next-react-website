d712c545a442bdd15c54502c3b53dd11d0a22e0b
n4tadecoco <t.yuya0812@icloud.com>
2023-12-15 16:31:01 +0900

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,10 +4,12 @@ import styles from 'styles/footer.module.css'
 export default function Footer() {
     return (
     <footer className={styles.wrapper}>
-        <div className={styles.flexContainer}
+        <Container>
+        <div className={styles.flexContainer}>
         <Logo  />
         [ソーシャル]
         </div>
+        </Container>
     </footer>
     )
 }
