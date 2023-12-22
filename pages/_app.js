80b862ca53b3c52d8bdf0ea0938bca8ffc26879f
n4tadecoco <t.yuya0812@icloud.com>
2023-12-22 16:30:22 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
+// Font Awesome の設定
+import '@fontawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fontawesome/fontawesome-svg-core'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
+
+export default MyApp
