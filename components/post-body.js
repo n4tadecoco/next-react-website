c07472d1ca5cc4207e7c6b562a59114545874d29
n4tadecoco <t.yuya0812@icloud.com>
2024-01-12 16:31:07 +0900

    Update post-body.js

--- components/post-body.js
+++ components/post-body.js
@@ -1,9 +1,18 @@
 import styles from 'styles/post-body.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
 
-export default function PostBody({ children }) {
+export default function PostHeader({ title, subtitle, publish = '' }) {
     return (
         <div className={styles.stack}>
-            {children}
+            <p className={styles.subtitle}>{subtitle}</p>
+            <h1 className={styles.title}>{title}</h1>
+            {publish && (
+            <div className={styles.publish}>
+                <FontAwesomeIcon icon={faClock} size="lg" color="var(--gray-25)" />
+                {publish}
+            </div>
+            )}
         </div>
     )
 }
