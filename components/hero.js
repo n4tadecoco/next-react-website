22602b86dd0ede122fba34fbb5a671acd6693090
n4tadecoco <t.yuya0812@icloud.com>
2024-01-12 16:31:13 +0900

    Update hero.js

--- components/hero.js
+++ components/hero.js
@@ -9,7 +9,7 @@ export default function Hero({ title, subtitle, image0n = false}) {
             </div>
             {image0n && (
             <figure className={styles.image}>
-                <image src={cube} alt="" layout="responsive"
+                <Image src={cube} alt="" layout="responsive"
                 sizes="(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vw"
                 priority
                 placeholder="blur"
