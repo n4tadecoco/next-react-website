b3fdb6d793574dccabe63852038ad5100ad51f1c
n4tadecoco <t.yuya0812@icloud.com>
2024-01-19 16:31:02 +0900

    Update schedule.js

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,6 +1,11 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
-import PostHeader from 'components/post-body'
+import PostHeader from 'components/post-header'
+import PostBody from 'components/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'components/two-column'
+import ConvertBody from 'components/post-body'
+import Image from 'next/image'
+import PostCategories from 'components/post-categories'
 
 export default function Schedule({
     title,
@@ -13,6 +18,29 @@ export default function Schedule({
     <Container>
         <article>
             <PostHeader title={title} subtitle="Blog Artcle" publish={publish} />
+
+            <figure>
+                <Image
+                src={eyecatch.url}
+                alt=""
+                layout="responsive"
+                width={eyecatch.width}
+                height={eyecatch.height}
+                sizes="(min-width: 1152px) 1152px, 100vm"
+                priority
+                />
+            </figure>
+
+            <TwoColumn>
+                <TwoColumnMain>
+                    <PostBody>
+                        <ConvertBody contentHTML={content} />
+                    </PostBody>
+                </TwoColumnMain>
+                <TwoColumnSidebar>
+                    <PostCategories categories={categories} />
+                </TwoColumnSidebar>
+            </TwoColumn>
         </article>
         </Container>
         )
