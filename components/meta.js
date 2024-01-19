69cb55ef508c079e375be696f9ce5b49d936bf4d
n4tadecoco <t.yuya0812@icloud.com>
2024-01-19 16:31:09 +0900

    Update meta.js

--- components/meta.js
+++ components/meta.js
@@ -8,7 +8,7 @@ const { siteTitle, siteDesc, siteUrl,  siteLocale, siteType, siteIcon } = siteMe
 // 汎用 OGP 画像
 import siteImg from 'images/ogp.jpg'
 
-export default function Meta({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) {
+export default function Meta({ pageTitle, pageDesc, }) {
     // ページのタイトル
     const title = pageTitle ? '${pageTitle} | ${siteTitle}' : siteTitle
 
@@ -17,13 +17,14 @@ export default function Meta({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH 
 
     //ページのURL
     const router = useRouter()
-    const url = '${siteUrl}${router.asPath}'
+    const url ='${siteUrl}${router.asPath}'
 
     // OGP 画像
     const img = pageImg || siteImg.src
     const imgW = pageImgW || siteImg.width
     const imgH = pageImgH || siteImg.height
     const imgUrl = img.startsWith('https') ? img : '${siteUrl}${img}'
+
     return (
         <Head>
             <title>{title}</title>
@@ -32,20 +33,20 @@ export default function Meta({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH 
             <meta name="description" content={desc} />
             <meta property="og:description" content={desc} />
 
-            <link rel="canonical" href={url} />
+            <Link rel="canonical" href={url} />
             <meta property="og:url" content={url} />
 
             <meta property="og:site_name" content={siteTitle} />
             <meta property="og:type" content={siteType} />
             <meta property="og:locale" content={siteLocale} />
 
-            <link rel="icon" href={siteIcon} />
-            <link rel="apple-touch-icon" href={siteIcon} />
+            <Link rel="icon" href={siteIcon} />
+            <Link rel="apple-touch-icon" href={siteIcon} />
 
             <meta property="og:image" content={imgUrl} />
             <meta property="og:image:width" content={imgW} />
             <meta property="og:image:height" content={imgH} />
-            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:card" content="summary_large_image"
         </Head>
     )
 }
